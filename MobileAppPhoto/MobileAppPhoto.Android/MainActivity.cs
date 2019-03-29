f3b04ddbdc0b63b9e6d99d5e79827b007b32dd33
Александр Краснов <sasha10101016@gmail.com>
2019-03-29 22:59:18 +0300

    Добавьте файлы проекта.

---
+++ MobileAppPhoto/MobileAppPhoto.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Plugin.Permissions;
+using Plugin.CurrentActivity;
+using MobileAppPhoto;
+
+namespace MediaSample.Droid
+{
+    [Activity(Label = "MediaSample",MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            CrossCurrentActivity.Current.Init(this, bundle);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
